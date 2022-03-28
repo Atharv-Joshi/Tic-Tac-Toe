ba694a2f93ea8fa13b336baca965b4168c67e641
Atharv-Joshi-TE <joshiatharv1078@protonmail.com>
2022-03-29 00:50:32 +0530

    changes in ui

--- lib/screens/choose_game_mode.dart
+++ lib/screens/choose_game_mode.dart
@@ -1,4 +1,6 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tic_tac_toe/helpers/function_calls.dart';
 import 'package:tic_tac_toe/models/player.dart';
 import 'package:tic_tac_toe/screens/local_two_player/ltw_game_screen.dart';
@@ -12,55 +14,62 @@ class ChooseGameMode extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.greenAccent,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width * 0.6,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.white,
+      body: Stack(
+        children: [
+          Image.asset('assets/Good.png'),
+          Positioned(
+            top:  MediaQuery.of(context).size.height * 0.55,
+            left: MediaQuery.of(context).size.width  * 0.2,
+            child: Column(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                      ),
+                      onPressed: (){
+                        List<List<String>> matrix = functionCalls.createLTPInitialMatrix();
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => LTPGameScreen(fMatrix: matrix)));
+                      }
+                      , child: const Text('Local Game', style: TextStyle(color: Colors.black),)
+                  ),
                 ),
-                  onPressed: (){
-                    List<List<String>> matrix = functionCalls.createLTPInitialMatrix();
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => LTPGameScreen(fMatrix: matrix)));
-                  }
-                  , child: const Text('Local Game', style: TextStyle(color: Colors.black),)
-              ),
-            ),
-            const SizedBox(height: 25,),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.6,
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.white,
+                const SizedBox(height: 25,),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                      ),
+                      onPressed: () async {
+                        BotToast.showSimpleNotification(title: 'Coming soon!');
+                        // final mapData = await functionCalls.createOTPInitialMatrix();
+                        // Player.value = 'O';
+                        // Navigator.pushReplacement(context,
+                        //     MaterialPageRoute(builder: (context) => InitiateStreamScreen(fMatrix: mapData["matrix"], uid: mapData['uid'])));
+                      }
+                      , child: const Text('Create Online Game', style: TextStyle(color: Colors.black),)
                   ),
-                  onPressed: () async {
-                    final mapData = await functionCalls.createOTPInitialMatrix();
-                    Player.value = 'O';
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) => InitiateStreamScreen(fMatrix: mapData["matrix"], uid: mapData['uid'])));
-                  }
-                  , child: const Text('Create Online Game', style: TextStyle(color: Colors.black),)
-              ),
+                ),
+                // const SizedBox(height: 25,),
+                // Container(
+                //   width: MediaQuery.of(context).size.width * 0.6,
+                //   child: ElevatedButton(
+                //       style: ElevatedButton.styleFrom(
+                //         primary: Colors.white,
+                //       ),
+                //       onPressed: () async {
+                //         Player.value = 'X';
+                //         Navigator.push(context, MaterialPageRoute(builder: (_) => SelectLobby()));
+                //       }
+                //       , child: const Text('Join Online Game', style: TextStyle(color: Colors.black),)
+                //   ),
+                // ),
+              ],
             ),
-            const SizedBox(height: 25,),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.6,
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
-                  ),
-                  onPressed: () async {
-                    Player.value = 'X';
-                    Navigator.push(context, MaterialPageRoute(builder: (_) => SelectLobby()));
-                  }
-                  , child: const Text('Join Online Game', style: TextStyle(color: Colors.black),)
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,3 +1,4 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:tic_tac_toe/screens/choose_game_mode.dart';
 
@@ -6,8 +7,10 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: BotToastInit(),
       debugShowCheckedModeBanner: false,
       home: ChooseGameMode(),
+      navigatorObservers: [BotToastNavigatorObserver()],
     );
   }
 }
